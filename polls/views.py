8f3b318c1d274d919185a950c20eaf80eba728be
ChristopheAnderson <christophewavoeke18@gmail.com>
2025-02-10 01:47:08 +0100

    test

--- polls/views.py
+++ polls/views.py
@@ -3,7 +3,16 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import HttpResponse
+from django.template import loader
+
+
+""" def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.") """
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    template = loader.get_template("polls/index.html")
+    context = {
+        "latest_question_list": "",
+    }
+    return HttpResponse(template.render(context, request))
